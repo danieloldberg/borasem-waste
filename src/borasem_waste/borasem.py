51b29ced2b9e4b7e4e6b8b0932a4dbd3254576c4
Daniel Oldberg <daniel.oldberg@danieloldberg.se>
2023-09-05 21:30:07 +0200

    fixed type error on containerId

--- src/borasem_waste/borasem.py
+++ src/borasem_waste/borasem.py
@@ -1,21 +1,16 @@
-import argparse
 import logging
-import sys
+from typing import List
 
 from borasem_waste import __version__
 
-from typing import List
-
-from . import auth
-from . import wastepickup
-from . import borasem_const
-import json
+from . import auth, borasem_const, wastepickup
 
 __author__ = "Daniel Oldberg"
 __copyright__ = "Daniel Oldberg"
 __license__ = "MIT"
 
 _logger = logging.getLogger(__name__)
+
 
 class BorasEM:
     """Class to communicate with the Borås Energi och Miljö Waste API."""
@@ -26,14 +21,32 @@ class BorasEM:
 
     async def async_get_schedule(self, address: str) -> List[wastepickup.WastePickup]:
         """Return the schedule of the containers."""
-        resp = await self.auth.request("get", (borasem_const.WASTE_PATH + "/" + borasem_const.SCHEDULE_PATH + "?" + borasem_const.ADDRESS_PARAM + "=" + address))
+        resp = await self.auth.request(
+            "get",
+            (
+                borasem_const.WASTE_PATH
+                + "/"
+                + borasem_const.SCHEDULE_PATH
+                + "?"
+                + borasem_const.ADDRESS_PARAM
+                + "="
+                + address
+            ),
+        )
         resp.raise_for_status()
         response = await resp.json()
-        return [wastepickup.WastePickup(schedule_data, self.auth) for schedule_data in response['RhServices']]
+        return [
+            wastepickup.WastePickup(schedule_data, self.auth)
+            for schedule_data in response["RhServices"]
+        ]
 
     async def async_get_address(self, address: str) -> List[str]:
         """Return any matching addresses."""
-        resp = await self.auth.request("post", (borasem_const.WASTE_PATH + "/" + borasem_const.ADDRESSSEARCH_PATH), json={'searchText': address })
+        resp = await self.auth.request(
+            "post",
+            (borasem_const.WASTE_PATH + "/" + borasem_const.ADDRESSSEARCH_PATH),
+            json={"searchText": address},
+        )
         resp.raise_for_status()
         response = await resp.json()
-        return response['Buildings']
+        return response["Buildings"]
--- src/borasem_waste/wastepickup.py
+++ src/borasem_waste/wastepickup.py
@@ -1,5 +1,5 @@
-from typing import List
 from . import auth
+
 
 class WastePickup:
     """Class that represents a Waste pickup in the Borås Energi och Miljö API."""
@@ -12,31 +12,31 @@ class WastePickup:
     # Note: each property name maps the name in the returned data
 
     @property
-    def containerId(self) -> str:
+    def container_id(self) -> str:
         """Return the ID (Kärl X) of the container."""
         return self.raw_data["WasteType"]
 
     @property
-    def NextWastePickup(self) -> str:
+    def next_waste_pickup(self) -> str:
         """Return the next pickup of the Waste container."""
         return self.raw_data["NextWastePickup"]
 
     @property
-    def WastePickupsPerYear(self) -> int:
+    def waste_pickups_per_year(self) -> int:
         """Return the number of pickups per year."""
         return self.raw_data["WastePickupsPerYear"]
-    
+
     @property
-    def WastePickupFrequency(self) -> str:
+    def waste_pickup_frequency(self) -> str:
         """Return the frequency of the pickups."""
         return self.raw_data["WastePickupFrequency"]
-    
+
     @property
-    def ContainerType(self) -> int:
+    def container_type(self) -> int:
         """Return the type of the containers."""
         return self.raw_data["Designation"]
-    
+
     @property
-    def IsActive(self) -> bool:
+    def is_active(self) -> bool:
         """Return the if the container delivery is active."""
         return self.raw_data["IsActive"]
