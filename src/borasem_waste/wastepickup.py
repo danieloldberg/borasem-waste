b8bbe239ddcb2d142ab844abfe8a834b1fad9993
Daniel Oldberg <daniel.oldberg@danieloldberg.se>
2023-09-05 22:21:31 +0200

    __getitem__

--- src/borasem_waste/wastepickup.py
+++ src/borasem_waste/wastepickup.py
@@ -1,42 +1,42 @@
-from . import auth
-
 
 class WastePickup:
     """Class that represents a Waste pickup in the Borås Energi och Miljö API."""
 
-    def __init__(self, raw_data: dict, auth: auth.Auth):
+    def __init__(self, _raw_data: dict):
         """Initialize a waste pickup instance."""
-        self.raw_data = raw_data
-        self.auth = auth
+        self._raw_data = _raw_data
+
+    def __getitem__(self, item):
+        return getattr(self, item)
 
     # Note: each property name maps the name in the returned data
 
     @property
     def container_id(self) -> str:
         """Return the ID (Kärl X) of the container."""
-        return self.raw_data["WasteType"]
+        return self._raw_data["WasteType"]
 
     @property
     def next_waste_pickup(self) -> str:
         """Return the next pickup of the Waste container."""
-        return self.raw_data["NextWastePickup"]
+        return self._raw_data["NextWastePickup"]
 
     @property
     def waste_pickups_per_year(self) -> int:
         """Return the number of pickups per year."""
-        return self.raw_data["WastePickupsPerYear"]
+        return self._raw_data["WastePickupsPerYear"]
 
     @property
     def waste_pickup_frequency(self) -> str:
         """Return the frequency of the pickups."""
-        return self.raw_data["WastePickupFrequency"]
+        return self._raw_data["WastePickupFrequency"]
 
     @property
     def container_type(self) -> int:
         """Return the type of the containers."""
-        return self.raw_data["Designation"]
+        return self._raw_data["Designation"]
 
     @property
     def is_active(self) -> bool:
         """Return the if the container delivery is active."""
-        return self.raw_data["IsActive"]
+        return self._raw_data["IsActive"]
